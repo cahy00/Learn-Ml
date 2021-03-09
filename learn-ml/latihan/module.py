b2cd8674e8a846e8c592593e3e174f2f82a6cccb
Cahyo <cariercahyo@gmail.com>
2021-03-09 22:02:31 +0900

    update learn-pandas

--- learn-ml/latihan/module.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-f = open('https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv', 'r')
-reader = csv.reader(f)
-
-for row in reader:
-	print(row)
--- learn-ml/latihan/pand.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-
-table = pd.read_csv("https://storage.googleapis.com/dqlab-dataset/penduduk_gender_head.csv")
-table.head()
-print(table)
